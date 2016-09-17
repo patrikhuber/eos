cb6dd3546289945a6d9fe35fc9c2df0680273e44
Patrik Huber <patrikhuber@users.noreply.github.com>
2016-09-17 21:19:46 +0100

    Added infrastructure to generate Python bindings
    - Added a flag GENERATE_PYTHON_BINDINGS to CMake which enables the use of pybind11
    - `create-python-bindings` generates bindings for PcaModel
    - pybind11 is added as a submodule; needs to be cloned with `git submodule update --init`
    - Increased cmake_minimum_required to 2.8.12 for pybind11

---
+++ utils/create-python-bindings.cpp
@@ -0,0 +1,37 @@
+/*
+ * eos - A 3D Morphable Model fitting library written in modern C++11/14.
+ *
+ * File: utils/create-python-bindings.cpp
+ *
+ * Copyright 2016 Patrik Huber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "eos/morphablemodel/PcaModel.hpp"
+
+#include "pybind11/pybind11.h"
+
+namespace py = pybind11;
+
+/**
+ * Create python bindings for the eos library using pybind11.
+ */
+PYBIND11_PLUGIN(eos_py) {
+    py::module m("eos_py", "Python bindings to the 3D Morphable Face Model fitting library");
+
+	py::class_<eos::morphablemodel::PcaModel>(m, "PcaModel")
+		.def(py::init<>())
+		.def("get_mean_at_point", &eos::morphablemodel::PcaModel::get_mean_at_point);
+
+    return m.ptr();
+};
