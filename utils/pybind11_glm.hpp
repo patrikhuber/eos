ca8aa7d79f2d33efccbc5b7db61a360ea0abf195
Patrik Huber <patrikhuber@users.noreply.github.com>
2016-10-04 17:36:09 +0100

    Added conversion for 3x4 matrices
    Careful about GLM's notation! A 3x4 matrix is a mat4x3!

--- utils/pybind11_glm.hpp
+++ utils/pybind11_glm.hpp
@@ -29,6 +29,9 @@ NAMESPACE_BEGIN(detail)
  *
  * All converters for matrices assume col-major storage of glm, the default.
  * Things will likely break if non-default storage order is used.
+ *
+ * Note: GLM follows the GLSL matrix definition, so e.g. a glm::tmat4x3 has 4 cols
+ * and 3 rows, and is thus (in the standard mathematical notation) a 3x4 matrix.
  */
 
 template<typename T, glm::precision P>
@@ -173,6 +176,60 @@ struct type_caster<glm::tvec4<T, P>>
 protected:
 	template <typename T = vector_type>
 	static PYBIND11_DESCR elements() { return _(std::to_string(num_elements).c_str()); }
+};
+
+template<typename T, glm::precision P>
+struct type_caster<glm::tmat4x3<T, P>>
+{
+	using matrix_type = glm::tmat4x3<T, P>;
+	typedef typename T Scalar;
+	static constexpr std::size_t num_rows = 3;
+	static constexpr std::size_t num_cols = 4;
+
+	bool load(handle src, bool)
+	{
+		array_t<Scalar> buf(src, true);
+		if (!buf.check())
+			return false;
+
+		if (buf.ndim() == 2) // a 2-dimensional matrix
+		{
+			if (buf.shape(0) != num_rows || buf.shape(1) != num_cols) {
+				return false; // not a 3x4 matrix
+			}
+			if (buf.strides(0) / sizeof(Scalar) != num_cols || buf.strides(1) != sizeof(Scalar))
+			{
+				std::cout << "An array with non-standard strides is given. Please pass a contiguous array." << std::endl;
+				return false;
+			}
+			// What we get from Python is laid out in row-major memory order, while GLM's
+			// storage is col-major, thus, we create a mat3x4 and then transpose.
+			value = glm::transpose(glm::make_mat3x4(buf.mutable_data())); // make_mat*() copies the data (unnecessarily)
+		}
+		else { // buf.ndim() != 2
+			return false;
+		}
+		return true;
+	}
+
+	static handle cast(const matrix_type& src, return_value_policy /* policy */, handle /* parent */)
+	{
+		return array(
+			{ num_rows, num_cols }, // shape
+			{ sizeof(Scalar), sizeof(Scalar) * num_rows }, // strides - flip the row/col layout!
+			glm::value_ptr(src)                            // data
+		).release();
+	}
+
+	// Specifies the doc-string for the type in Python:
+	PYBIND11_TYPE_CASTER(matrix_type, _("numpy.ndarray[") + npy_format_descriptor<Scalar>::name() +
+		_("[") + rows() + _(", ") + cols() + _("]]"));
+
+protected:
+	template <typename T = matrix_type>
+	static PYBIND11_DESCR rows() { return _(std::to_string(num_rows).c_str()); }
+	template <typename T = matrix_type>
+	static PYBIND11_DESCR cols() { return _(std::to_string(num_cols).c_str()); }
 };
 
 template<typename T, glm::precision P>
