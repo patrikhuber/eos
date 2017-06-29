a34b38216af41b3b8f68f3040b783e9e2020c28e
Patrik Huber <patrikhuber@gmail.com>
2017-06-29 19:39:50 +0100

    Split type casters into Image3u and Image4u
    Also removed all old comments.
    
    There's now quite a bit of code duplication, but both should be implemented correctly now, for both ways (Python<=>C++).
    We can make this more generic later.

--- python/pybind11_Image.hpp
+++ python/pybind11_Image.hpp
@@ -24,6 +24,7 @@
 #include "Eigen/Core"
 
 #include <cstddef>
+#include <vector>
 
 NAMESPACE_BEGIN(pybind11)
 NAMESPACE_BEGIN(detail)
@@ -41,27 +42,19 @@ NAMESPACE_BEGIN(detail)
 
 
 /**
- * @brief Transparent conversion for eos::core::Image to and from Python.
+ * @brief Transparent conversion for eos::core::Image3u to and from Python.
  *
- * Converts eos::core::Image's to and from Python. Can construct a eos::core::Image from numpy arrays,
+ * Converts an eos::core::Image3u to and from Python. Can construct a eos::core::Image3u from numpy arrays,
  * as well as potentially other Python array types.
  *
- * //- Supports only contiguous matrices
- * //- The numpy array has to be in default (row-major) storage order
- * //- Non-default strides are not implemented.
+ * Note: Not sure what happens if the given numpy array is not contiguous, not in default (row-major) storage
+ *       order, or has non-standard strides. It may or may not work.
  */
-template<class T, int num_channels>
-struct type_caster<eos::core::Image<T, num_channels>>
+template<>
+struct type_caster<eos::core::Image3u>
 {
-	using image_type = eos::core::Image<T, num_channels>;
-	//using Scalar = T;
-	//static constexpr std::size_t num_elements = N;
-	
 	bool load(handle src, bool)
 	{
-		// Since cv::Mat has its time dynamically specified at run-time, we can't bind functions
-		// that take a cv::Mat to any particular Scalar type.
-		// Thus the data we get from python can be any type.
 		auto buf = pybind11::array::ensure(src);
 		if (!buf)
 			return false;
@@ -74,62 +67,104 @@ struct type_caster<eos::core::Image<T, num_channels>>
 		}
 
 		if (buf.ndim() != 3) {
-			return false;
+			return false; // we expected a numpy array with 3 dimensions.
 		}
 		// We got something with 3 dimensions, i.e. an image with 2, 3 or 4 channels (or 'k' for that matter):
-		auto num_chans = buf.shape(2); // Check whether 3 or 4 and abort otherwise??
-		auto shp = buf.shape();
-		auto str = buf.strides();
-		// Now: If our Image class had support for col/row major, we could just map buf.mutable_data().
+		if (buf.shape(2) != 3) {
+			return false; // We expected a 3-channel image.
+		}
+		
+		// Note: If our Image class had support for col/row major, we could just map buf.mutable_data().
 		// Like with OpenCV: value = cv::Mat(buf.shape(0), buf.shape(1), opencv_type, buf.mutable_data());
-		// But since it doesn't, we just copy for now:
-		value = core::Image3u(buf.shape(0), buf.shape(1));
-		array_t<std::uint8_t> test(buf);
+		// But since it doesn't, we just copy the data for now:
+		value = eos::core::Image3u(buf.shape(0), buf.shape(1));
+		array_t<std::uint8_t> buf_as_array(buf);
 		for (int r = 0; r < buf.shape(0); ++r) {
 			for (int c = 0; c < buf.shape(1); ++c) {
-				value(r, c)[0] = test.at(r, c, 0);
-				value(r, c)[1] = test.at(r, c, 1);
-				value(r, c)[2] = test.at(r, c, 2);
+				value(r, c)[0] = buf_as_array.at(r, c, 0);
+				value(r, c)[1] = buf_as_array.at(r, c, 1);
+				value(r, c)[2] = buf_as_array.at(r, c, 2);
 			}
 		}
 		return true;
 	};
 
-    static handle cast(const image_type& src, return_value_policy /* policy */, handle /* parent */)
+    static handle cast(const eos::core::Image3u& src, return_value_policy /* policy */, handle /* parent */)
 	{
+		const std::size_t num_channels = 3;
+		std::vector<std::size_t> shape = { src.rows, src.cols, num_channels };
 
-		//const auto opencv_depth = src.depth(); // CV_8U etc - the element type
-		const auto num_chans = 4;
-		std::vector<std::size_t> shape;
-		if (num_chans == 1)
-		{
-			shape = { (size_t)src.rows, (size_t)src.cols };
-			// if either of them is == 1, we could specify only 1 value for shape - but be careful with strides,
-			// if there's a col-vector, I don't think we can do it without using strides.
-			// Also, check what happens in python when we pass a col & row vec respectively.
-		}
-		else if (num_chans == 2 || num_chans == 3 || num_chans == 4)
-		{
-			shape = { (size_t)src.rows, (size_t)src.cols, (size_t)num_chans };
-		}
-		else {
-			throw std::runtime_error("Cannot return matrices with more than 4 channels back to Python.");
-			// We could probably implement this quite easily but >4 channel images/matrices don't occur often.
-		}
-
-		// Now return the data, depending on its type:
-		//if (opencv_depth == CV_8U) // if element type = uint8_t...
-		//{
-			// (2048, 4, 1) is default which results in transposed image
-			std::vector<size_t> strides = { 4, 2048, 1 }; // This works now. In numpy the strides are (2048, 4, 1) though. I think a copy gets created nevertheless?
-			return array(pybind11::dtype::of<std::uint8_t>(), shape, strides, &src.data[0]).release();
-		//}
-		//else {
-		//	throw std::runtime_error("Can currently only return matrices of type 8U back to Python. Other types can be added if needed.");
-		//}
+		// (2048, 4, 1) is default which results in transposed image
+		// Below line works now. In numpy the strides are (2048, 4, 1) though. I think a copy gets created nevertheless?
+		std::vector<std::size_t> strides = { num_channels, num_channels * src.rows, 1 }; // might be cols or rows...? I think rows?
+		return array(pybind11::dtype::of<std::uint8_t>(), shape, strides, &src.data[0]).release();
 	};
 
-    PYBIND11_TYPE_CASTER(image_type, _("numpy.ndarray[uint8|int32|float32|float64[m, n, d]]"));
+    PYBIND11_TYPE_CASTER(eos::core::Image3u, _("numpy.ndarray[uint8[m, n, 3]]"));
+};
+
+/**
+ * @brief Transparent conversion for eos::core::Image4u to and from Python.
+ *
+ * Converts an eos::core::Image4u to and from Python. Can construct a eos::core::Image4u from numpy arrays,
+ * as well as potentially other Python array types.
+ *
+ * Note: Not sure what happens if the given numpy array is not contiguous, not in default (row-major) storage
+ *       order, or has non-standard strides. It may or may not work.
+ */
+template<>
+struct type_caster<eos::core::Image4u>
+{
+	bool load(handle src, bool)
+	{
+		auto buf = pybind11::array::ensure(src);
+		if (!buf)
+			return false;
+
+		// Todo: We should probably check that buf.strides(i) is "default", by dividing it by the Scalar type or something.
+
+		if (!pybind11::isinstance<pybind11::array_t<std::uint8_t>>(buf))
+		{
+			return false; // we only convert uint8_t for now.
+		}
+
+		if (buf.ndim() != 3) {
+			return false; // we expected a numpy array with 3 dimensions.
+		}
+		// We got something with 3 dimensions, i.e. an image with 2, 3 or 4 channels (or 'k' for that matter):
+		if (buf.shape(2) != 4) {
+			return false; // We expected a 4-channel image.
+		}
+
+		// Note: If our Image class had support for col/row major, we could just map buf.mutable_data().
+		// Like with OpenCV: value = cv::Mat(buf.shape(0), buf.shape(1), opencv_type, buf.mutable_data());
+		// But since it doesn't, we just copy the data for now:
+		value = eos::core::Image4u(buf.shape(0), buf.shape(1));
+		array_t<std::uint8_t> buf_as_array(buf);
+		for (int r = 0; r < buf.shape(0); ++r) {
+			for (int c = 0; c < buf.shape(1); ++c) {
+				value(r, c)[0] = buf_as_array.at(r, c, 0);
+				value(r, c)[1] = buf_as_array.at(r, c, 1);
+				value(r, c)[2] = buf_as_array.at(r, c, 2);
+				value(r, c)[3] = buf_as_array.at(r, c, 3);
+			}
+		}
+		return true;
+	};
+
+    static handle cast(const eos::core::Image4u& src, return_value_policy /* policy */, handle /* parent */)
+	{
+		const std::size_t num_chanels = 4;
+		std::vector<std::size_t> shape;
+		shape = { src.rows, src.cols, num_chanels };
+
+		// (2048, 4, 1) is default which results in transposed image
+		// Below line works now. In numpy the strides are (2048, 4, 1) though. I think a copy gets created nevertheless?
+		std::vector<size_t> strides = { num_chanels, num_chanels * src.rows, 1 }; // might be cols or rows...? I think rows?
+		return array(pybind11::dtype::of<std::uint8_t>(), shape, strides, &src.data[0]).release();
+	};
+
+    PYBIND11_TYPE_CASTER(eos::core::Image4u, _("numpy.ndarray[uint8[m, n, 4]]"));
 };
 
 NAMESPACE_END(detail)
