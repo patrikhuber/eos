92d91e2ee4e04b5b3734a9570653be2916535757
Patrik Huber <patrikhuber@gmail.com>
2017-07-08 11:43:14 +0100

    Removed setup.py code for the external libraries (boost/opencv/eigen)
    The Python package now builds completely dependency-free (requiring only >=gcc-7).

--- setup.py
+++ setup.py
@@ -17,27 +17,6 @@ class CMakeExtension(Extension):
 
 class CMakeBuild(build_ext):
 
-    # user_options are best specified in setup.cfg, this works for both pip and 'python setup.py'.
-    # Via command-line, it might be possible like this:
-    #     setup.py: $ python setup.py --help build_ext
-    #     For pip, it might be something like that but it doesn't work:
-    #         $ pip install .\eos --build-option="--opencv-dir=C:/opencv/install"
-    # So just use setup.cfg instead.
-    # -----
-    # The second argument is an optional short option (can e.g. be 'b').
-    # It seems options need to be lower-case.
-    user_options = build_ext.user_options + [
-        ('boost-root', None, "Boost search location"),
-        ('eigen3-include-dir', None, "Directory of the Eigen3 headers"),
-        ('opencv-dir', None, "OpenCV config dir, where OpenCVConfig.cmake can be found")
-    ]
-
-    def initialize_options(self):
-        build_ext.initialize_options(self)
-        self.boost_root = None
-        self.eigen3_include_dir = None
-        self.opencv_dir = None
-
     def run(self):
         try:
             out = subprocess.check_output(['cmake', '--version'])
@@ -60,18 +39,12 @@ class CMakeBuild(build_ext):
                       '-DEOS_BUILD_EXAMPLES=OFF',
                       '-DEOS_GENERATE_PYTHON_BINDINGS=ON'
                       ]
-        if self.opencv_dir:
-            cmake_args += ['-DOpenCV_DIR:PATH=' + self.opencv_dir]
-        if self.boost_root:
-            cmake_args += ['-DBOOST_ROOT:PATH=' + self.boost_root]
-        if self.eigen3_include_dir:
-            cmake_args += ['-DEIGEN3_INCLUDE_DIR:PATH=' + self.eigen3_include_dir]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ['-G', 'Visual Studio 14 2015 Win64']
+            cmake_args += ['-G', 'Visual Studio 15 2017 Win64']
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             build_args += ['--', '/m']
         else:
@@ -88,7 +61,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='eos-py',
-    version='0.12.1',
+    version='0.12.99-alpha1',
     author='Patrik Huber',
     author_email='patrikhuber@gmail.com',
     description='Python bindings for eos - A lightweight 3D Morphable Face Model fitting library in modern C++11/14',
