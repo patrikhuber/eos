85f411dd4ea9358598b7c5aece141b416003976e
Patrik Huber <patrikhuber@gmail.com>
2017-10-20 16:54:44 +0100

    Exposed LandmarkMapper::convert function to Python

--- python/generate-python-bindings.cpp
+++ python/generate-python-bindings.cpp
@@ -63,7 +63,7 @@ PYBIND11_MODULE(eos, eos_module) {
 	py::class_<core::LandmarkMapper>(core_module, "LandmarkMapper", "Represents a mapping from one kind of landmarks to a different format(e.g.model vertices).")
 		.def(py::init<>(), "Constructs a new landmark mapper that performs an identity mapping, that is, its output is the same as the input.")
 		.def(py::init<std::string>(), "Constructs a new landmark mapper from a file containing mappings from one set of landmark identifiers to another.", py::arg("filename"))
-		// We can't expose the convert member function yet - need std::optional (or some trick with self/this and a lambda)
+                .def("convert", &core::LandmarkMapper::convert, "Converts the given landmark name to the mapped name.", py::arg("landmark_name"))
 		;
 
 	py::class_<core::Mesh>(core_module, "Mesh", "This class represents a 3D mesh consisting of vertices, vertex colour information and texture coordinates.")
