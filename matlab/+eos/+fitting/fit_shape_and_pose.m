8e78f2ad006b3ab976ce0be8ed276ed4a18da98d
Patrik Huber <patrikhuber@users.noreply.github.com>
2018-05-02 21:52:16 +0100

    Updated default sfm_model_contours filename in Matlab bindings

--- matlab/+eos/+fitting/fit_shape_and_pose.m
+++ matlab/+eos/+fitting/fit_shape_and_pose.m
@@ -34,7 +34,7 @@ end
 % provided:
 if (~exist('edge_topology', 'var')), edge_topology = '../share/sfm_3448_edge_topology.json'; end
 if (~exist('contour_landmarks', 'var')), contour_landmarks = '../share/ibug_to_sfm.txt'; end
-if (~exist('model_contour', 'var')), model_contour = '../share/model_contours.json'; end
+if (~exist('model_contour', 'var')), model_contour = '../share/sfm_model_contours.json'; end
 if (~exist('num_iterations', 'var')), num_iterations = 5; end
 if (~exist('num_shape_coefficients_to_fit', 'var')), num_shape_coefficients_to_fit = -1; end
 if (~exist('lambda', 'var')), lambda = 30.0; end
