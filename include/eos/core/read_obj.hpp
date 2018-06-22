c0ad893410563da432cb9983d3df9cacebdd7b98
Patrik Huber <patrikhuber@users.noreply.github.com>
2018-06-22 22:57:59 +0100

    Removed commented string split function

--- include/eos/core/read_obj.hpp
+++ include/eos/core/read_obj.hpp
@@ -39,29 +39,6 @@ namespace core {
 
 namespace detail {
 
-/*
-// Split on a single delimiter.
-template <typename Out>
-void split(const std::string& s, char delim, Out result)
-{
-    std::stringstream ss;
-    ss.str(s);
-    std::string item;
-    while (std::getline(ss, item, delim))
-    {
-        // Can use an if here and push back for behaviour like token_compress. See https://stackoverflow.com/questions/236129/most-elegant-way-to-split-a-string/236803#236803.
-        *(result++) = item;
-    }
-}
-
-inline std::vector<std::string> split(const std::string& s, char delim)
-{
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
-*/
-
 // From: https://stackoverflow.com/a/1493195/1345959
 template <class ContainerType>
 void tokenize(const std::string& str, ContainerType& tokens, const std::string& delimiters = " ",
