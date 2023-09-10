41dfa510fca1df57fa679ddb63b889f030017884
Omar Omar <omarhatem5555@gmail.com>
2023-09-10 10:29:01 +0100

    fixing the path to the model

--- tests/test_load_model.py
+++ tests/test_load_model.py
@@ -1,7 +1,8 @@
 import pytest
 import eos
 import numpy as np
+from pathlib import Path
 
 def test_load_model():
-    model = eos.morphablemodel.load_model("../share/sfm_shape_3448.bin")
+    model = eos.morphablemodel.load_model(str(Path(__file__).resolve().parent.parent / 'share' / 'sfm_shape_3448.bin'))
     assert model is not None
